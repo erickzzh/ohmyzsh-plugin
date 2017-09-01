b065e0586240fc548c7b3b38dd79f43b8ef290c4
Erick <erickzzh@gmail.com>
2017-08-31 17:55:42 -0700

    Add files via upload

---
+++ espn.plugin.zsh
@@ -0,0 +1,47 @@
+function espn() {
+  emulate -L zsh
+
+showHelp () {
+    echo "  [league name] scoreboard                         # gets the scoredboard for today";
+    echo "  [league name] standings                          # gets the standing of the league";
+    echo "  [league name] schedule                           # gets the game schedule";
+
+}
+  if [ $# = 0 ]; then
+    showHelp;
+  else
+    # declear an array of searches
+    typeset -A urls
+    urls=(
+        nba         "http://www.espn.com/nba/"
+        mlb         "http://www.espn.com/mlb/"
+        nfl         "http://www.espn.com/nfl/"
+        ncaaf       "http://www.espn.com/college-football/"
+    )
+    #     check if the leagua is supported
+    if [[ -z "$urls[$1]" ]]; then
+        echo "league $1 not supported."
+        return 1
+    fi
+
+    # check for the number of param
+    if [[ $# -eq 1 ]]; then
+        # build search url:
+        # join arguments passed with '+', then append to search engine URL
+        url="${urls[$1]}${(j:+:)@[2,-1]}"
+    else
+        # build main page url:
+        # split by '/', then rejoin protocol (1) and domain (2) parts with '//'
+        url="${(j://:)${urls[$1]}}"
+    fi
+
+
+    open_command "$url"
+  fi
+}
+
+
+alias nba='espn nba'
+alias mlb='espn mlb'
+alias nfl='espn nfl'
+alias ncaaf='espn ncaaf'
